00296522496febc84da1f05b17e157720f212f8b
abdullahalhamidi <abdullahalhamidi@outlook.com>
2022-07-18 13:09:45 -0500

    Add project files.

---
+++ DotNet6Crud/Controllers/AppointmentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DotNet6Crud.Controllers
+{
+    public class AppointmentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
