6848218ce9e6828642859d58bcd7ac62a218fd7b
abdullahalhamidi <abdullahalhamidi@outlook.com>
2022-07-18 18:50:57 -0500

    Created controllers and views to handle page routing

---
+++ DotNet6Crud/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using DotNet6Crud.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNet6Crud.Data
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Item> Items { get; set; }
+
+    }
+}
---
+++ DotNet6Crud/Models/Item.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DotNet6Crud.Models
+{
+    public class Item
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Borrower { get; set; }
+
+    }
+}
